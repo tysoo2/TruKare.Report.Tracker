1fe98ac64f34dc10328989553e39d1f078cf9ef8
Tyson <tysonh@trukare.com>
2025-12-22 11:35:05 -0700

    Added swagger packages

--- TruKare.Reports/Program.cs
+++ TruKare.Reports/Program.cs
@@ -1,10 +1,12 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -12,6 +14,9 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI();
+    app.UseSwagger():
+
 }
 
 app.UseHttpsRedirection();
